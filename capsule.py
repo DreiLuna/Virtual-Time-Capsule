a15364a4c42d5e6fc2305c4145a088892107512e
Kevin Yang <kimbap@m3air.local>
2025-10-09 15:08:04 -0500

    Seperate lock from capsule

--- capsule.py
+++ capsule.py
@@ -4,12 +4,3 @@ class Time_Capsule:
     def __init__(self, date, userID):
         self.set_lock(date)
         self.user = userID
-
-    def set_lock(self, date):
-        # encrypt
-        return
-
-    def unlock(self, date):
-        if (datetime.now().date() == date):
-            # decrypt
-            return
