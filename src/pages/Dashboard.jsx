c3bf09807732bc4167cb32cc709141d938ab3f95
Drei Luna <dreifire@gmail.com>
2025-10-10 15:05:33 -0500

    started working on cpasules on dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,54 @@
 import { useAuth } from "../auth/AuthContext";
 
+//^ importing assets
+import photo1 from "../assets/temp/photo1.jpg"
+import photo2 from"../assets/temp/photo2.jpg"
+import photo3 from"../assets/temp/photo3.jpg"
+import photo4 from"../assets/temp/photo4.jpg"
+const images = [
+      photo1,
+      photo2,
+      photo3,
+      photo4
+  ];
+const capsuleName = "New Capsule"
+
 //$ importing css
-import "../css/Landing.css"
+import "../css/dashboard.css"
 
 export default function Dashboard() {
   const { user, logout } = useAuth();
 
   return (
-    <div className="page">
-      <div className="topbar">
-        <h1 className="title">Welcome, {user?.name}!</h1>
-        <button onClick={logout} className="btn">Log out</button>
-      </div>
+    <div className="main">
+      <nav className = "navbar">
 
-      <p className="container muted">
+      {/* //title */}
+      <ul className="title">
+
+        <h1 className="topTitle">Welcome, {user?.name}!</h1>
+        <i><h5 className="underTitle">
         This is a protected page. Only visible when authenticated.
-      </p>
+        </h5></i>
+
+      </ul>
+      {/* //button */}
+      <button onClick={logout} className="logoutbtn">Log out</button>
+
+      </nav>
+
+
+      <div className="content">
+        <div className="capsuleDiv">
+          {images.map((src, index) => (
+            <div className="seperateCapsules">
+              <img key={index} src={src} alt={`Image ${index}`}/>
+              <h2>{capsuleName}</h2>
+            </div>
+        ))}
+        </div>
+      </div>
+      
     </div>
   );
 }
