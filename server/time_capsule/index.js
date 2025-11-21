587f666b90690067bb67e7a56c4848b2d5fb1491
Kevin Yang <kimbap@m3air.local>
2025-11-21 12:37:42 -0600

    Rename files

--- server/time_capsule/index.js
+++ server/time_capsule/index.js
@@ -4,9 +4,50 @@ const app = express();
 // Import the route files
 import userRoutes from './routes/users.js'
 
-app.use(express.json());
-app.use('/api/users', userRoutes);
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
 
-// Run backend
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Running on ${port}...`));
+app.listen(port, () => console.log(`Listening on ${port}...`));
+
+// // Register
+// app.post("/register", (req, res) => {
+
+// });
+
+
+
+import { Sequelize, Model, DataTypes } from 'sequelize';
+
+
+
+
+export const sequelize = new Sequelize({
+  dialect: 'postgres',
+  database: 'postgres',
+  user: 'postgres',
+  password: 'mysecretpassword',
+  host: 'localhost',
+  port: 5432,
+  ssl: true,
+  clientMinMessages: 'notice',
+});
+
+class User extends Model {}
+User.init(
+  {
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+  },
+  { sequelize, modelName: 'user' },
+);
+
+(async () => {
+  await sequelize.sync();
+  const jane = await User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20),
+  });
+  console.log(jane.toJSON());
+})();
