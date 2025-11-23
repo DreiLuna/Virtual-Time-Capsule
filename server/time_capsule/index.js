ea5ccb9ee52191656ec4c1ed6bba8d8b74c3e19c
Kevin Yang <kimbap@m3air.local>
2025-11-23 13:29:20 -0600

    delete comments

--- server/time_capsule/index.js
+++ server/time_capsule/index.js
@@ -32,33 +32,3 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}...`));
-
-
-// export const sequelize = new Sequelize({
-//   dialect: 'postgres',
-//   database: 'postgres',
-//   user: 'postgres',
-//   password: 'mysecretpassword',
-//   host: 'localhost',
-//   port: 5432,
-//   ssl: true,
-//   clientMinMessages: 'notice',
-// });
-
-// class User extends Model {}
-// User.init(
-//   {
-//     username: DataTypes.STRING,
-//     birthday: DataTypes.DATE,
-//   },
-//   { sequelize, modelName: 'user' },
-// );
-
-// (async () => {
-//   await sequelize.sync();
-//   const jane = await User.create({
-//     username: 'janedoe',
-//     birthday: new Date(1980, 6, 20),
-//   });
-//   console.log(jane.toJSON());
-// })();
