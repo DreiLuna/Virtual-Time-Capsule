86a60403d413e77bd753476d9363e81dd142049f
Kevin Yang <kimbap@m3air.local>
2025-11-23 21:08:22 -0600

    add test

--- server/time_capsule/__tests__/users.test.js
+++ server/time_capsule/__tests__/users.test.js
@@ -1,7 +1,20 @@
-import app from '../index.js';
+import { createUserHandler } from "../handlers/users.js";
 
-describe('register', () => {
-    it('Return OK after successful registration', () => {
+jest.mock('express-validator', () => ({
+    validationResult: jest.fn( () => ({
+        isEmpty: jest.fn(() => false),
+        array: jest.fn(() => [{msg: "Invalid Username"}]),
+    })),
+}));
 
+describe('create users', () => {
+    it('Return error when field is empty', async () => {
+        const mockRequest = {};
+        const mockResponse = {
+            sendStatus: jest.fn(),
+            send: jest.fn(),
+            status: jest.fn( () => mockResponse),
+        }
+        await createUserHandler(mockRequest,mockResponse);
     })
 });
